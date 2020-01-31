1c960e2f482c3a34f68d00834ccc9f60e9835890
The Promised Prince <debapriya.das.blr@gmail.com>
2020-01-31 17:49:42 +0530

    learning to export go funcs as C

--- lookup.go
+++ lookup.go
@@ -5,24 +5,48 @@ import (
 	"fmt"
 	"strings"
 	"net/http"
+	"os"
 )
 
+//export CallIPLookup
+func CallIPLookup(myval *C.char)  *C.char{
+	// this is an exportable funciton
+	/*
+	this is a comment
+	*/
+	fmt.Println(C.GoString(myval))
+	return myval
 
-func callIPLookup() {
+}
+
+//export BlehBleh
+func BlehBleh() {
 	
 }
 
 func main() {
+	os.Setenv("FOO", "1")
+    fmt.Println("FOO:", os.Getenv("FOO"))
+    fmt.Println("BAR:", os.Getenv("BAR"))
+
+    fmt.Println()
+    for _, e := range os.Environ() {
+        pair := strings.SplitN(e, "=", 2)
+        fmt.Println(pair)
+	}
+	CallIPLookup(C.CString("abcd"))
 	client := &http.Client{
 		//CheckRedirect: redirectPolicyFunc,
 	}
 	
 	resp, err := client.Get("http://example.com")
+	
+	fmt.Println(resp.Body, err)
 	// ...
 	
-	req, err := http.NewRequest("GET", "http://example.com", nil)
-	// ...
-	req.Header.Add("If-None-Match", `W/"wyzzy"`)
-	resp, err := client.Do(req)
+	// req, err := http.NewRequest("GET", "http://example.com", nil)
+	// // ...
+	// req.Header.Add("If-None-Match", `W/"wyzzy"`)
+	// resp, err := client.Do(req)
 
 }
